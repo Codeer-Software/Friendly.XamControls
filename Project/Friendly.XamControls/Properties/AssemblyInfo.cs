fce68e1c11acaaa34445ab1ba94fd045b7296bdc
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2019-06-18 00:04:52 +0900

    Version up dependency packages.

--- Project/Friendly.XamControls/Properties/AssemblyInfo.cs
+++ Project/Friendly.XamControls/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("e1191c67-8b5f-410b-b870-951c0870927d")]
 
-[assembly: AssemblyVersion("1.5.5.0")]
-[assembly: AssemblyFileVersion("1.5.5.0")]
+[assembly: AssemblyVersion("1.5.7.0")]
+[assembly: AssemblyFileVersion("1.5.7.0")]
